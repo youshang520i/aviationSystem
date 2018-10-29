b227c02e4e4dc65ab5950a0f4c2a3aceb19d2688
304808680@qq.com <1314520.liao>
2018-10-29 15:42:58 +0800

    测试Git版本冲突

--- src/Test/java/Git.java
+++ src/Test/java/Git.java
@@ -10,6 +10,7 @@ public class Git {
     @Test
     public void test(){
         System.out.println("youshang520i:1323");
+        System.out.println("youshang520i：在zhijun没提交前我更新测试一下冲突");
 
     }
 }
