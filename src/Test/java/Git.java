7c86d92d71581a847fd9e33ec8d29eeb6d62769c
304808680@qq.com <1314520.liao>
2018-10-29 11:49:39 +0800

    测试Git

---
+++ src/Test/java/Git.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * @actuor youshang520i
+ * @create 2018-10-29 11:43
+ * @desc 测试Git
+ */
+public class Git {
+
+    @Test
+    public void test(){
+        System.out.println("youshang520i:1323");
+
+    }
+}
